01f27f3b3b102e576d6f9f1082acfbfc071182c2
Rapptoor <davidscaletchirei@gmail.com>
2024-01-22 11:12:56 +0000

    adicionados ficheiros do projeto

--- enviar_email.php
+++ enviar_email.php
@@ -1,26 +1,20 @@
 <?php
 
-require 'caminho/para/PHPMailerAutoload.php';
+
+require_once '../lib/swift_required.php';
 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $carta = $_POST["carta"];
-    $destinatario = "jedsonpeido@gmail.com";
-    $assunto = "Carta de Music Finder";
+$transport = Swift_SmtpTransport::newInstance('mail.esl11ui.website', 25)
+->setUsername('david@esl11ui.website')
+->setPassword('g4bn{Q_lw8g9')
+;
+$mailer = Swift_Mailer::newInstance($transport);
+ 
 
-    // Adicione cabeçalhos necessários, como "From", "Reply-To", etc., conforme necessário
+$message = Swift_Message::newInstance('ola')
+->setFrom(array('david@esl11ui.website' => 'Scaletchi'))
+->setTo(array('a701621765@es-loule.edu.pt'))
+->setBody('TESTE E-MAIL PHP - A funcionar')
+;
 
-    // Enviar o e-mail
-    $enviado = mail($destinatario, $assunto, $carta);
-
-    if ($enviado) {
-        echo "Carta enviada com sucesso!";
-    } else {
-        echo "Falha ao enviar a carta. Por favor, tente novamente.";
-    }
-} else {
-    // Se alguém tentar acessar este script diretamente, retorna um erro
-    http_response_code(400);
-    echo "Erro de solicitação inválida.";
-}
-?>
+$result = $mailer->send($message);
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e40d4a0c47f0cad4397e053f62d8348730639e2c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e40d4a0c47f0cad4397e053f62d8348730639e2c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
